7498d32d88a3e1adea019dd101be5364a5427b21
Micah Best <micahbest21@gmail.com>
2024-02-19 12:39:10 -0600

    Added execution_time.py

---
+++ src/pytools-qol/execution_time.py
@@ -0,0 +1,28 @@
+import time
+
+def print_execution_time(start_time) -> None:
+    """
+    Prints the time it took to run a program after execution.
+
+    Args:
+        - start_time (float) : start time of the program (`start_time = time.time()`)
+
+    Returns:
+        - None
+    """
+    assert type(start_time) is float
+    exec_time = time.time() - start_time
+
+    nseconds = exec_time % 60
+    nminutes = exec_time // 60
+    nhours = exec_time // (60*60)
+    ndays = exec_time // (60*60*24)
+
+    if ndays:
+        print(f"Program executed in {ndays:.0f} days, {nhours:.0f} hours, {nminutes:.0f} minutes, {nseconds:.0f} seconds.")
+    elif nhours:
+        print(f"Program executed in {nhours:.0f} hours, {nminutes:.0f} minutes, {nseconds:.0f} seconds.")
+    elif nminutes:
+        print(f"Program executed in {nminutes:.0f} minutes, {nseconds:.0f} seconds.")
+    else:
+        print(f"Program executed in {nseconds:.0f} seconds.")
